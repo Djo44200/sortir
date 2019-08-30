c8e397a08088872beb0651e9c12ad36347068c06
jbodet2019 <jonathan.bodet2019@campus-eni.fr>
2019-08-30 08:51:19 +0200

    MAJ des recherches

--- src/Form/SortieRechercheType.php
+++ src/Form/SortieRechercheType.php
@@ -4,18 +4,14 @@
 namespace App\Form;
 
 
-use App\Entity\Lieu;
 use App\Entity\Site;
 use App\Entity\Sortie;
-use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -34,9 +30,43 @@ class SortieRechercheType extends AbstractType
             ])
             ->add('search',SearchType::class,[
                 'label' => ' Le nom de la sortie contient :',
-                'attr'=> array('class'=>'form-control mr-sm-2', 'placeholder'=>'Recherche')
+                'attr'=> array('class'=>'form-control mr-sm-2', 'placeholder'=>'Recherche'),
+                'required'=>false
             ])
-
+            ->add('dateDebut',DateType::class,[
+                'label' => ' Entre  ',
+                'widget'=>'single_text',
+                'required'=>false
+            ])
+            ->add('dateFin',DateType::class,[
+                'label' => ' et ',
+                'widget'=>'single_text',
+                'required'=>false
+            ])
+            ->add('sortieOrganisateur',CheckboxType::class,[
+                'trim'=>true,
+                'label'=> 'Sortie dont je suis l\'organisateur',
+                'attr'=> array('class'=>'form-check-label'),
+                'required'=>false
+            ])
+            ->add('sortieInscris',CheckboxType::class,[
+                'trim'=>true,
+                'label'=> 'Sortie auquelles je suis inscris',
+                'attr'=> array('class'=>'form-check-label'),
+                'required'=>false
+            ])
+            ->add('sortieNonInscris',CheckboxType::class,[
+                'trim'=>true,
+                'label'=> 'Sortie auquelles je ne suis pas inscris',
+                'attr'=> array('class'=>'form-check-label'),
+                'required'=>false
+            ])
+            ->add('sortiePassees',CheckboxType::class,[
+                'trim'=>true,
+                'label'=> 'Sortie passées',
+                'attr'=> array('class'=>'form-check-label'),
+                'required'=>false
+            ])
 
 
 
