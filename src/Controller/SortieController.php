fad07332609bd17911013b5a771a36407fa19024
jbodet2019 <jonathan.bodet2019@campus-eni.fr>
2019-09-05 16:13:56 +0200

    Gestion des pages d'erreurs

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -42,16 +42,7 @@ class SortieController extends Controller
         $userInscris = $form->get('userInscris')->getData();
         $userNonInscri = $form->get('userNonInscris')->getData();
         $sortiePassee = $form->get('sortiePassee')->getData();
-        //Check des sorties à supprimer
-        $listeSortieACloturer = $entityManager->getRepository('App:Sortie')->rechercheParCloture();
-        // Mettre la liste des sorties en état PAS
-        if ($listeSortieACloturer) {
-            foreach ($listeSortieACloturer as $sortie) {
-                $sortie->setEtat('PAS');
-                $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->flush();
-            }
-        }
+
             $userId = $this->get('security.token_storage')->getToken()->getUser()->getId();
             if ($form->isSubmitted() && $form->isValid()) {
                 if ($site || $search || $dateDebut || $dateFin || $userOrgan || $userInscris || $userNonInscri || $sortiePassee) {
